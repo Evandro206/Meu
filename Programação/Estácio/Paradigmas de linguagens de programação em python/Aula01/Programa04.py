c7074cbe2d821bf790e28cf0f6e74418fdd95216
Evandro Machado <164398238+PomboHunter@users.noreply.github.com>
2024-10-08 13:12:08 -0300

    Rename programa04.py to Programa04.py

---
+++ Programação/Estácio/Paradigmas de linguagens de programação em python/Aula01/Programa04.py
@@ -0,0 +1,1 @@
+print(f"O resultado são {float(input('Digite quantos metros você deseja transformar: ')) * 100} cm")
--- Programação/Estácio/Paradigmas de linguagens de programação em python/Aula01/programa04.py
+++
@@ -1,1 +0,0 @@
-print(f"O resultado são {float(input('Digite quantos metros você deseja transformar: ')) * 100} cm")
